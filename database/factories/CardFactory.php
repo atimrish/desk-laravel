2aa08038cb0fbe0aca683ee26949dd1f33e9e630
atimrish <atimrish11@gmail.com>
2023-02-05 20:02:32 +0400

    first commit

---
+++ database/factories/CardFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\DeskList;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Card>
+ */
+class CardFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+            'name' => $this->faker->name,
+            'desk_list_id' => DeskList::inRandomOrder()->first()->id
+        ];
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Card;
+use App\Models\Desk;
+use App\Models\DeskList;
+use App\Models\Task;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        Desk::factory(10)->create();
+        DeskList::factory(10)->create();
+        Card::factory(10)->create();
+        Task::factory(10)->create();
+
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
